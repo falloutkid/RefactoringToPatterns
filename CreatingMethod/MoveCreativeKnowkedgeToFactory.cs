2e172fc9baa120d2c451cd1b01ba3ff28cd4847c
Acky <sppw93f9@angel.ocn.ne.jp>
2014-04-10 03:35:50 +0900

    Factoryによる生成関数の書き換えスタート

---
+++ CreatingMethod/MoveCreativeKnowkedgeToFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternOrientedRefactoring
+{
+    class Parser
+    {
+        public Boolean ShouldDecode { set; get; }
+        public Boolean ShouldRemoveEscapeCharactor { set; get; }
+    }
+
+    public interface Node { }
+
+    class StringParser
+    {
+        public Node find(StringBuilder textBuffer, int textBegin, int textEnd, bool shouldDecode)
+        {
+            Parser parser = new Parser();
+            parser.ShouldDecode = shouldDecode;
+            return StringNode.createStringNode(textBuffer, textBegin, textEnd, parser.ShouldDecode);
+        }
+        class DecodingStringNode : Node
+        {
+            public DecodingStringNode(StringNode string_node) { }
+        }
+        class StringNode : Node
+        {
+            StringNode(StringBuilder textBuffer, int textBegin, int textEnd)
+            {
+            }
+            public static Node createStringNode(StringBuilder textBuffer, int textBegin, int textEnd, Boolean shouldDecode)
+            {
+                if (shouldDecode)
+                    return new DecodingStringNode(new StringNode(textBuffer, textBegin, textEnd));
+                return new StringNode(textBuffer, textBegin, textEnd);
+            }
+
+        }
+    }
+}
