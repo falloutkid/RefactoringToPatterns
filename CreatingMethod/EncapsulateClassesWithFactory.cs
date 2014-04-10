1095fb25a82dbcc9b879ba179f91fe760e7bc3be
Acky <sppw93f9@angel.ocn.ne.jp>
2014-04-11 03:51:07 +0900

    初期状態

---
+++ CreatingMethod/EncapsulateClassesWithFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternOrientedRefactoring
+{
+    public abstract class AttributeDescriptor
+    {
+        protected AttributeDescriptor() { }
+    }
+
+
+    public class BooleanDescriptor : AttributeDescriptor
+    {
+        public BooleanDescriptor()
+            : base()
+        {
+        }
+    }
+
+    public class DefaultDescriptor : AttributeDescriptor
+    {
+        public DefaultDescriptor()
+            : base()
+        {
+        }
+    }
+
+    public class ReferenceDescriptor : AttributeDescriptor
+    {
+        public ReferenceDescriptor()
+            : base()
+        {
+        }
+    }
+}
