a6fbc036a496b65861ae161302829a8509e679a0
Acky <sppw93f9@angel.ocn.ne.jp>
2014-04-12 21:15:29 +0900

    全テストパス

--- CreatingMethod/EncapsulateCompositeWithBuilder.cs
+++ CreatingMethod/EncapsulateCompositeWithBuilder.cs
@@ -56,5 +56,38 @@ namespace PatternOrientedRefactoring
             doc.FirstChild.AppendChild(element);
             current = doc.FirstChild.LastChild;
         }
+
+        public void addAttribute(string p1, string p2)
+        {
+            XmlAttribute attr = doc.CreateAttribute(p1);
+            attr.Value = p2;
+            current.Attributes.Append(attr);
+        }
+
+        public void addToParent(string parent, string child)
+        {
+            XmlNode search_node = findParent(parent);
+            if (search_node == null)
+                return;
+            current = search_node;
+            addChild(child);
+        }
+
+        private XmlNode findParent(string parent_prefix)
+        {
+            XmlNode search_node = current;
+            while(search_node != null)
+            {
+                if (parent_prefix == search_node.Name)
+                    return search_node;
+                search_node = search_node.ParentNode;
+            }
+            return null;
+        }
+
+        public void addValue(string p)
+        {
+            current.AppendChild(doc.CreateTextNode(p));
+        }
     }
 }
