fc90958bc3d7ec8a169db2288a54cde4ec823426
Acky <sppw93f9@angel.ocn.ne.jp>
2014-04-12 14:36:37 +0900

    スタート

---
+++ CreatingMethod/IntroducePolymorphicCreation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternOrientedRefactoring
+{
+    public interface OutputBuilder
+    {
+        void addBelow(string tagName);
+        void addAbove(string tagName);
+    }
+
+    class DOMBuilder : OutputBuilder
+    {
+        string tag_name_;
+        public DOMBuilder(string tagName)
+        {
+            tag_name_ = tagName;
+        }
+        public void addBelow(string tagName) { }
+        public void addAbove(string tagName) { }
+
+    }
+
+    public class DOMBuilderTest
+    {
+        private OutputBuilder builder;
+
+        public void testAddAboveRoot()
+        {
+            String invalidResult =
+            "<orders>" +
+              "<order>" +
+              "</order>" +
+            "</orders>" +
+            "<customer>" +
+            "</customer>";
+            builder = new DOMBuilder("orders");  // used to be new XMLBuilder("orders")
+            builder.addBelow("order");
+            try
+            {
+                builder.addAbove("customer");
+                System.Diagnostics.Debug.Assert(false, "expecting Exception");
+            }
+            catch (Exception ignored)
+            {
+                System.Diagnostics.Debug.WriteLine(ignored.Message.ToString());
+            }
+        }
+    }
+}
