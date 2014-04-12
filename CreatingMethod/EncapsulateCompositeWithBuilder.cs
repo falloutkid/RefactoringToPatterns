72d8ec2847b46d20c6168fd86b659f84cf3b63e8
Acky <sppw93f9@angel.ocn.ne.jp>
2014-04-12 19:37:36 +0900

    第一テストパス

---
+++ CreatingMethod/EncapsulateCompositeWithBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml;
+
+
+namespace PatternOrientedRefactoring
+{
+    public class TagBuilder
+    {
+        private XmlDocument doc;
+        XmlNode root;
+        public TagBuilder(String rootTagName)
+        {
+            doc = new XmlDocument();
+            doc.AppendChild(doc.CreateElement(rootTagName));
+            root = doc.FirstChild;
+        }
+
+        public String toXml()
+        {
+            return doc.InnerXml;
+        }
+
+        public void addChild(string childTagName)
+        {
+//            TagNode parentNode = currentNode;
+//            currentNode = new TagNode(childTagName);
+//            parentNode.add(currentNode);
+        }
+    }
+}
--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -2,13 +2,14 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using SampleCode;
+using PatternOrientedRefactoring;
 
 using Moq;
 
 namespace UnitTestProject
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestSampleCode
     {
         public TestContext TestContext { set; get; }
 
@@ -17,6 +18,31 @@ namespace UnitTestProject
         {
             HelloWorld obj = new HelloWorld();
             obj.writeHelloWorld();
+        }
+    }
+
+    [TestClass]
+    public class UnitTestCreate
+    {
+        public TestContext TestContext { set; get; }
+
+        [TestMethod]
+        public void TestTagBuilerFirst()
+        {
+            String expectedXml = "<flavors />";
+            String actualXml = new TagBuilder("flavors").toXml();
+            Assert.AreEqual(expectedXml, actualXml);
+        }
+
+        [TestMethod]
+        public void TestTagBuilerOneChild()
+        {
+            String expectedXml = "<flavors>" + "<flavor />" + "</flavors>";
+
+            TagBuilder builder = new TagBuilder("flavors");
+            builder.addChild("flavor");
+            String actualXml = builder.toXml();
+            Assert.AreEqual(expectedXml, actualXml);
         }
     }
 }
