a9cf62125d3775f1c81a97479950fb55c11e736b
Acky <sppw93f9@angel.ocn.ne.jp>
2014-04-26 12:48:21 +0900

    リファクタリング開始

---
+++ PatternOrientedRefactoringCommodity/ExtractComposite.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternOrientedRefactoringCommodity
+{
+    public class Tag { }
+    public class Node
+    {
+        public string PlainTextString { set; get; }
+    }
+    public class LinkTag : Tag
+    {
+        private List<Node> linkData;
+        public String toPlainTextString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Node node in linkData)
+            {
+                sb.Append(node.PlainTextString);
+            }
+            return sb.ToString();
+        }
+    }
+
+    public class FormTag : Tag
+    {
+        protected List<Node> allNodesList;
+        public String toPlainTextString()
+        {
+            StringBuilder stringRepresentation = new StringBuilder();
+            foreach (Node node in allNodesList)
+            {
+                stringRepresentation.Append(node.PlainTextString);
+            }
+            return stringRepresentation.ToString();
+        }
+    }
+}
