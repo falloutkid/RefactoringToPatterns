5b7c39fd8ca2f19a2a53cf1ebd7f59ccca9186b6
Acky <sppw93f9@angel.ocn.ne.jp>
2014-04-28 03:49:41 +0900

    リファクタリング開始

---
+++ PatternOrientedRefactoringCommodity/ReplaceOneOrManyDistinctionsWithComposite.cs
@@ -0,0 +1,153 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternOrientedRefactoringCommodity
+{
+    public class ProductRepository
+    {
+        List<Product> product_lists;
+
+        public ProductRepository()
+        {
+            product_lists = new List<Product>();
+        }
+
+        public void add(Product product)
+        {
+            product_lists.Add(product);
+        }
+
+        public List<Product> selectBy(ColorSpec colorSpec)
+        {
+            List<Spec> specs = new List<Spec>();
+            specs.Add(colorSpec);
+
+            return selectBy(specs);
+            /*
+            List<Product> return_list = new List<Product>();
+
+            foreach(Product product in product_lists)
+            {
+                if (product.Color == colorSpec.Color)
+                    return_list.Add(product);
+            }
+
+            return return_list;
+            */
+        }
+
+        public List<Product> selectBy(List<Spec> specs)
+        {
+            List<Product> return_list = new List<Product>();
+
+            foreach (Product product in product_lists)
+            {
+                bool is_match = true;
+                foreach(Spec spec in specs)
+                {
+                    if(spec.GetType() == typeof(ColorSpec))
+                    {
+                        if(spec.SpecValue != product.Color)
+                            is_match = false;
+                    }
+                    else if(spec.GetType() == typeof(SizeSpec))
+                    {
+                         if(spec.SpecValue != product.Size)
+                            is_match = false;
+                    }
+                    else if (spec.GetType() == typeof(BelowPriceSpec))
+                    {
+                        if (spec.Price != product.Price)
+                            is_match = false;
+                    }
+                }
+                if (is_match)
+                    return_list.Add(product);
+            }
+
+            return return_list;
+        }
+    }
+
+    public class Product
+    {
+        private string id_;
+        private string name_;
+        private string color_;
+        private float price_;
+        private string size_;
+
+        public string ID { get { return id_; } }
+        public string Name { get { return name_; } }
+        public string Color { get { return color_; } }
+        public float Price { get { return price_; } }
+        public string Size { get { return size_; } }
+
+        public Product(string id, string name, string color, float price, string size)
+        {
+            this.id_ = id;
+            this.name_ = name;
+            this.color_ = color;
+            this.price_ = price;
+            this.size_ = size;
+        }
+    }
+
+    public class Color
+    {
+        public static readonly string red = "red";
+        public static readonly string yellow = "yellow";
+        public static readonly string pink = "pink";
+        public static readonly string white = "white";
+    }
+
+    public class ProductSize
+    {
+        public static readonly string MEDIUM = "MEDIUM";
+        public static readonly string SMALL = "SMALL";
+        public static readonly string LARGE = "LARGE";
+        public static readonly string NOT_APPLICABLE = "NOT_APPLICABLE";
+    }
+
+    public class Spec
+    {
+        protected string spec = "";
+        protected float price = 0.0F;
+        public string SpecValue{get{return spec;}}
+        public float Price { get { return price; } }
+    }
+
+    public class ColorSpec:Spec
+    {
+        public string Color { get { return spec; } }
+
+        public ColorSpec(string color)
+        {
+            this.spec = color;
+        }
+    }
+
+    public class SizeSpec : Spec
+    {
+        public string Size { get { return spec; } }
+
+        public SizeSpec(string size)
+        {
+            this.spec = size;
+        }
+    }
+    
+    public class BelowPriceSpec : Spec
+    {
+        public float Price { get { return price; } }
+
+        public BelowPriceSpec(float price)
+        {
+            this.price = price;
+        }
+    }
+}
--- UnitTestProject/UnitTestCommodity.cs
+++ UnitTestProject/UnitTestCommodity.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Collections.Generic;
 using PatternOrientedRefactoringCommodity;
 
 namespace UnitTestProject
@@ -17,6 +17,53 @@ namespace UnitTestProject
             Loan loan = new Loan();
             System.Diagnostics.Debug.WriteLine("csa capital : " + csa.capital(loan));
             System.Diagnostics.Debug.WriteLine("cst capital : " + cst.capital(loan));
+        }
+    }
+
+    [TestClass]
+    public class ProductRepositoryTest
+    {
+        private ProductRepository repository;
+        Product fireTruck;
+        Product barbieClassic;
+        Product frisbee;
+        Product baseball;
+        Product toyConvertible;
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            repository = new ProductRepository();
+            fireTruck = new Product("f1234", "Fire Truck", Color.red, 8.95f, ProductSize.MEDIUM);
+            barbieClassic = new Product("b7654", "Barbie Classic", Color.yellow, 15.95f, ProductSize.SMALL);
+            frisbee = new Product("f4321", "Frisbee", Color.pink, 9.99f, ProductSize.LARGE);
+            baseball = new Product("b2343", "Baseball", Color.white, 8.95f, ProductSize.NOT_APPLICABLE);
+            toyConvertible = new Product("p1112", "Toy Porsche Convertible", Color.red, 230.00f, ProductSize.NOT_APPLICABLE);
+            repository.add(fireTruck);
+            repository.add(barbieClassic);
+            repository.add(frisbee);
+            repository.add(baseball);
+            repository.add(toyConvertible);
+        }
+
+        [TestMethod()]
+        public void testFindByColor()
+        {
+            List<Product> foundProducts = repository.selectBy(new ColorSpec(Color.red));
+            Assert.AreEqual(2, foundProducts.Count, "found 2 red products");
+            Assert.IsTrue(foundProducts.Contains(fireTruck), "found fireTruck");
+            Assert.IsTrue(foundProducts.Contains(toyConvertible), "found Toy Porsche Convertible");
+        }
+
+        [TestMethod]
+        public void testFindByColorSizeAndBelowPrice()
+        {
+            List<Spec> specs = new List<Spec>();
+            specs.Add(new ColorSpec(Color.red));
+            specs.Add(new SizeSpec(ProductSize.SMALL));
+            specs.Add(new BelowPriceSpec((float)10.00));
+            List<Product> foundProducts = repository.selectBy(specs);
+            Assert.AreEqual(0, foundProducts.Count, "small red products below .00");
         }
     }
 }
